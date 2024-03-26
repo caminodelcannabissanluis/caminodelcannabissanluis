23f50c5094650d048475f187a19b0c098ce64651
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2024-03-26 19:24:15 -0300

    asd

--- src/app/BlogCard.jsx
+++ src/app/BlogCard.jsx
@@ -69,7 +69,7 @@ export default () => {
                       <article className="max-w-md mx-auto mt-4 shadow-lg border rounded-md duration-300 hover:shadow-sm" key={key}>
                           <a href={items.href}>
 
-                              <img src={items.img} loading="lazy" alt={items.title}  className="w-full h-full rounded-t-md" />
+                              <img src={items.img} loading="lazy" alt={items.title}  className="w-full h-48 rounded-t-md" />
 
 
                               <div className="pt-3 ml-4 mr-2 mb-3">
