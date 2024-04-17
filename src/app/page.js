bbbb612fe5b6dac0b7a4c118ee61976c10369042
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2024-04-17 16:08:16 -0300

    analitcs

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 "use client"
 import Image from "next/image";
 import BlogCard from "./BlogCard";
-
+import Script from "next/script";
 import Head from 'next/head';
 
 
@@ -14,9 +14,36 @@ export default function Home() {
 
   
   return (
+ 
+
 
     <main className="flex  pattern-isometric pattern-blue-100 pattern-bg-white 
     pattern-size-20 pattern-opacity-100 min-h-screen flex-col items-center justify-between p-4">
+
+
+
+
+<Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}',{
+          page_path: window.location.pathname,
+          
+});
+         `}
+      </Script>
+
+
+
+
+
 
       <Head>
         <title>El Camino del Cannabis en San Luis</title>
